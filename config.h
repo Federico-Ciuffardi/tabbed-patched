35a78bdb752984b6b7f14ae497289905c88ddeba
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2020-10-19 02:48:37 -0300

    config update

--- config.h
+++ config.h
@@ -39,10 +39,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,     XK_Return, focusonce,   { 0 } },
 	{ MODKEY|ShiftMask,     XK_Return, spawn,       { 0 } },
 
-	{ MODKEY|ShiftMask,     XK_j,      rotate,      { .i = +1 } },
-	{ MODKEY|ShiftMask,     XK_k,      rotate,      { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_h,      movetab,     { .i = -1 } },
-	{ MODKEY|ShiftMask,     XK_l,      movetab,     { .i = +1 } },
+	{ MODKEY|ShiftMask,     XK_t     , spawn,       { 0 } },
+
+	{ MODKEY|ShiftMask,     XK_h,      rotate,     { .i = -1 } },
+	{ MODKEY|ShiftMask,     XK_l,      rotate,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
@@ -57,10 +57,9 @@ static Key keys[] = {
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY|ShiftMask,     XK_q,      killclient,  { 0 } },
+	{ MODKEY|ShiftMask,     XK_w,      killclient,  { 0 } },
 
 	{ MODKEY,               XK_t,      focusurgent, { 0 } },
 	{ MODKEY|ShiftMask,     XK_t,      toggle,      { .v = (void*) &urgentswitch } },
-
-	{ 0,                    XK_F11,    fullscreen,  { 0 } },
 };
